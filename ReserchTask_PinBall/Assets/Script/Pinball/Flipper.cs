56fc3993a32bfe0c894b86e580ac4966078d95a6
DESKTOP-CMOH7M4\blueg <blueg0826@gmail.com>
2020-09-03 12:18:20 +0900

    フリッパーを作った

---
+++ ReserchTask_PinBall/Assets/Script/Pinball/Flipper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+
+    [SerializeField] float spring = 40000;
+    [SerializeField] float openAngle = 60; // 開く角度
+    [SerializeField] float closeAngle = 0; // 閉じる角度
+
+
+    [SerializeField]HingeJoint _hingeJoint;
+    JointSpring _jointSpring;
+
+    private void Start()
+    {
+        _jointSpring = _hingeJoint.spring;
+    }
+    
+
+    public void FlipOn()
+    {
+        _jointSpring.spring = spring;
+        _jointSpring.targetPosition = openAngle;
+        _hingeJoint.spring = _jointSpring;
+    }
+
+    public void FlipOff()
+    {
+        _jointSpring.spring = spring;
+        _jointSpring.targetPosition = closeAngle;
+        _hingeJoint.spring = _jointSpring;
+    }
+}
---
+++ ReserchTask_PinBall/Assets/Script/Pinball/Test/HandTest/Test_Flipper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Flipper : MonoBehaviour
+{
+    [SerializeField] Flipper _flipper;
+    [SerializeField,Range(0,1)] int _clickSide;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(_clickSide))
+        {
+            _flipper.FlipOn();
+        }
+        if (Input.GetMouseButtonUp(_clickSide))
+        {
+            _flipper.FlipOff();
+        }
+    }
+}
