da52a6d601f119e43c02da0c3e0c1ca10dd935d6
DESKTOP-CMOH7M4\blueg <blueg0826@gmail.com>
2020-09-07 19:03:01 +0900

    弾を打てるようになった

---
+++ ReserchTask_PinBall/Assets/Script/Pinball/Shotter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotter : MonoBehaviour
+{
+    [SerializeField] GameObject _ballPrefab;
+    [SerializeField] Transform _createPos;
+    [SerializeField] Vector2 _direction;
+    [SerializeField] float _power;
+
+    [ContextMenu("Shot")]
+    public void Shot(GameObject obj)
+    {
+        var rb = obj.GetComponent<Rigidbody>();
+        rb.AddForce(_direction.normalized * _power, ForceMode.Impulse);
+    }
+    
+    public GameObject SetBall()
+    {
+        var create = Instantiate(_ballPrefab, _createPos.position, Quaternion.identity);
+        return create;
+    }
+}
---
+++ ReserchTask_PinBall/Assets/Script/Pinball/Test/HandTest/Test_shotter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_shotter : MonoBehaviour
+{
+    [SerializeField] Shotter _shotter;
+    [SerializeField] GameObject _target;
+
+    [ContextMenu("shot")]
+    void Test_shot()
+    {
+        _shotter.Shot(_target);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _target= _shotter.SetBall();
+            _shotter.Shot(_target);
+        }
+    }
+}
